ad306e5de57d14d349a8b3755c903a381d0cedb4
gzg_99@sina.com <YYBJ@192.168.129.241>
2018-06-23 17:21:03 +0800

    提交

--- src/com/yq/util/StringUtil.java
+++ src/com/yq/util/StringUtil.java
@@ -2,24 +2,23 @@ package com.yq.util;
 
 import javax.servlet.http.HttpSession;
 
-public class StringUtil {
+public class StringUtil
+{
+  protected Integer pagesize_1 = Integer.valueOf(10);
 
-	protected Integer pagesize_1 = 10;
+  public String getOppen_id(HttpSession session) {
+    return (String)session.getAttribute("oppen_id");
+  }
 
-	public String getOppen_id(HttpSession session) {
-		return (String) session.getAttribute("oppen_id");
-	}
-	
-	public void setOppen_id(String oppen_id, HttpSession session) {
-		session.setAttribute("oppen_id", "otwhkwERAfrnEXpO7Yh79vC6SX2A");
-	}
+  public void setOppen_id(String oppen_id, HttpSession session) {
+    session.setAttribute("oppen_id", oppen_id);
+  }
 
-	public Integer getPagesize_1() {
-		return pagesize_1;
-	}
+  public Integer getPagesize_1() {
+    return this.pagesize_1;
+  }
 
-	public void setPagesize_1(Integer pagesize_1) {
-		this.pagesize_1 = pagesize_1;
-	}
-
+  public void setPagesize_1(Integer pagesize_1) {
+    this.pagesize_1 = pagesize_1;
+  }
 }
