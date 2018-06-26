11f1539b6fd377389bfbc98cce70214bb2282227
gzg_99@sina.com <YYBJ@192.168.129.241>
2018-06-26 20:52:38 +0800

    tijiao

--- src/com/weixin/controller/ReplyController.java
+++ src/com/weixin/controller/ReplyController.java
@@ -1,157 +1,131 @@
 ﻿package com.weixin.controller;
 
+import com.weixin.entity.ClickText;
+import com.weixin.entity.Item;
+import com.weixin.entity.RequestTextMessage;
+import com.weixin.service.ClickTextService;
+import com.weixin.util.ReplyMessage;
+import com.weixin.util.SHA1;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
-import com.weixin.entity.ClickText;
-import com.weixin.entity.Item;
-import com.weixin.entity.RequestTextMessage;
-import com.weixin.service.ClickTextService;
-import com.weixin.util.MenuManager;
-import com.weixin.util.ReplyMessage;
-import com.weixin.util.SHA1;
-
 @Controller
-@RequestMapping("/")
+@RequestMapping({"/"})
 public class ReplyController {
-	private static final Logger log =Logger.getLogger(ReplyController.class);
-	private String TOKEN = "yqkj";
-	
-	@Autowired
-	private ClickTextService clickTextService;
-	
-	@RequestMapping(value = "reply.html")
-	public void repaly(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		checkToken(request, response); //仅在认证微信开发者模式时调用一次即可
-		try {
-			response.setContentType("text/html;charset=UTF-8");
-			PrintWriter pw = response.getWriter();
-			String wxMsgXml =IOUtils.toString(request.getInputStream(), "utf-8");
-			
-			log.info("获取的数据信息>>>>>"+wxMsgXml);
-		
-			boolean eventType =wxMsgXml.contains("Event") ;// 如果包含，则是触发事件
-			RequestTextMessage textMsg =null ;
-			String returnXml = null;
-			StringBuffer replyMsg = new StringBuffer();
-			if(!eventType){ //信息交互事件
-				textMsg = ReplyMessage.getRequestTextMessage(wxMsgXml);
-				String receive = textMsg.getContent().trim();
-			
-				ClickText clickText = clickTextService.selectByPrimaryKey(receive);
-				if(clickText != null) {
-					if (clickText.getType()==1) {
-						replyMsg.append(clickText.getContent());
-						returnXml = ReplyMessage.getReplyTextMessage(replyMsg.toString(), textMsg.getFromUserName(),
-							textMsg.getToUserName());
-					}else if(clickText.getType()==2){
-						List<Item> articleList = new ArrayList<>();
-						Item item = null ;
-						if(clickText.getIntro().contains("-=")){//多条图文信息
-							String[] title =  clickText.getTitle().split("-=");
-							String[] intro =  clickText.getIntro().split("-=");
-							String[] pic_url =  clickText.getPic_url().split("-=");
-							String[] url =  clickText.getUrl().split("-=");
-							for(int i =0 ; i < intro.length; i++){
-								item = new Item();
-								item.setTitle(title[i]); //标题
-								item.setDescription(intro[i]);//介绍
-								item.setPicUrl(pic_url[i]);	//图片链接
-								item.setUrl(url[i]);        //链接指向
-								articleList.add(item);
-							}
-						}else{ //单条图文信息
-							item = new Item();
-							item.setTitle(clickText.getTitle()); //标题
-							item.setDescription(clickText.getIntro());//介绍
-							item.setPicUrl(clickText.getPic_url());	//图片链接
-							item.setUrl(clickText.getUrl());        //链接指向
-							articleList.add(item);
-						}
-						returnXml = ReplyMessage.getReplyTuwenMessage(textMsg.getFromUserName(), textMsg.getToUserName(),articleList);
-					}
-				} else{
-					returnXml = ReplyMessage.getReplyTextMessage(getMainMenu(), textMsg.getFromUserName(),
-							textMsg.getToUserName());
-				}
-			}else{
-				textMsg = ReplyMessage.getRequestFocus(wxMsgXml);
-				if(textMsg.getEvent().equals("subscribe")){ //关注事件
-					ClickText clickText = clickTextService.selectByPrimaryKey("subscribe");
-					System.out.println();
-					replyMsg.append(clickText.getContent());
-					log.info("关注回复->"+clickText.getContent());
-					returnXml = 
-							ReplyMessage.getReplyTextMessage(replyMsg.toString(), textMsg.getFromUserName(),
-									textMsg.getToUserName());
-				}
-			}
-			log.info("wxMsgXml>>>>>>>>>>>>>>"+wxMsgXml);
-			pw.println(returnXml);
-		
-		} catch (Exception e) {
-			log.info(e.getMessage());
-		}
-	}
-	
-	public String getMainMenu() {
-		StringBuffer buffer = new StringBuffer();
-		buffer.append("您好，我是家滴帮客服，请回复数字选择服务：").append("\n\n");
-		buffer.append("1  天气预报").append("\n");
-		buffer.append("2  公交查询").append("\n");
-		buffer.append("3  周边搜索").append("\n");
-		buffer.append("4  歌曲点播").append("\n");
-		buffer.append("5  经典游戏").append("\n");
-		buffer.append("6  美女电台").append("\n");
-		buffer.append("7  人脸识别").append("\n");
-		buffer.append("8  聊天唠嗑").append("\n\n");
-		buffer.append("回复“?”显示此帮助菜单");
-		return buffer.toString();
-	}
-	
-	/**
-	 * 微信token认证，使用时调用
-	 * 
-	 * @param request
-	 * @param response
-	 * @throws IOException
-	 */
-	
-	public void checkToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// 微信加密签名
-		String signature = request.getParameter("signature");
-		// 随机字符串
-		String echostr = request.getParameter("echostr");
-		// 时间戳
-		String timestamp = request.getParameter("timestamp");
-		// 随机数
-		String nonce = request.getParameter("nonce");
+  private static final Logger log = Logger.getLogger(ReplyController.class);
+  private String TOKEN = "bjjkkj";
 
-		String[] str = { TOKEN, timestamp, nonce };
-		Arrays.sort(str); // 字典序排序
-		String bigStr = str[0] + str[1] + str[2];
-		// SHA1加密
-		String digest = new SHA1().getDigestOfString(bigStr.getBytes()).toLowerCase();
+  @Autowired
+  private ClickTextService clickTextService;
 
-		// 确认请求来至微信
-		if (digest.equals(signature)) {
-			response.getWriter().print(echostr);
-		}
-		MenuManager.create();
-	}
+  @RequestMapping({"reply.html"})
+  public void repaly(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { try { request.setCharacterEncoding("UTF-8");
+      response.setCharacterEncoding("UTF-8");
+      response.setHeader("Content-Type", "text/plain;charset=UTF-8");
+      String wxMsgXml = IOUtils.toString(request.getInputStream(), "utf-8");
+
+      log.info("获取的数据信息>>>>>" + wxMsgXml);
+
+      boolean eventType = wxMsgXml.contains("Event");
+      RequestTextMessage textMsg = null;
+      String returnXml = null;
+      StringBuffer replyMsg = new StringBuffer();
+      if (!eventType) {
+        textMsg = ReplyMessage.getRequestTextMessage(wxMsgXml);
+        String receive = textMsg.getContent().trim();
+        ClickText clickText = this.clickTextService.selectByPrimaryKey(receive);
+        if (clickText != null)
+          if (clickText.getType().intValue() == 1) {
+            replyMsg.append(clickText.getContent());
+            returnXml = ReplyMessage.getReplyTextMessage(replyMsg.toString(), textMsg.getFromUserName(), 
+              textMsg.getToUserName());
+          } else if (clickText.getType().intValue() == 2) {
+            List articleList = new ArrayList();
+            Item item = null;
+            if (clickText.getIntro().contains("-=")) {
+              String[] title = clickText.getTitle().split("-=");
+              String[] intro = clickText.getIntro().split("-=");
+              String[] pic_url = clickText.getPic_url().split("-=");
+              String[] url = clickText.getUrl().split("-=");
+              for (int i = 0; i < intro.length; i++) {
+                item = new Item();
+                item.setTitle(title[i]);
+                item.setDescription(intro[i]);
+                item.setPicUrl(pic_url[i]);
+                item.setUrl(url[i]);
+                articleList.add(item);
+              }
+            } else {
+              item = new Item();
+              item.setTitle(clickText.getTitle());
+              item.setDescription(clickText.getIntro());
+              item.setPicUrl(clickText.getPic_url());
+              item.setUrl(clickText.getUrl());
+              articleList.add(item);
+            }
+            returnXml = ReplyMessage.getReplyTuwenMessage(textMsg.getFromUserName(), textMsg.getToUserName(), articleList);
+          }
+      }
+      else {
+        textMsg = ReplyMessage.getRequestFocus(wxMsgXml);
+        if ("21".equals(textMsg.getEventKey())) {
+          returnXml = ReplyMessage.getReplyTextMessage(getMainMenu(), textMsg.getFromUserName(), 
+            textMsg.getToUserName());
+        }
+        if (textMsg.getEvent().equals("subscribe")) {
+          ClickText clickText = this.clickTextService.selectByPrimaryKey("subscribe");
+          replyMsg.append(clickText.getContent());
+          log.info("关注回复->" + clickText.getContent());
+          returnXml = ReplyMessage.getReplyTextMessage(replyMsg.toString(), textMsg.getFromUserName(), 
+            textMsg.getToUserName());
+        }
+      }
+      log.info("wxMsgXml>>>>>>>>>>>>>>" + wxMsgXml);
+      response.getOutputStream().write(returnXml.getBytes("UTF-8"));
+    } catch (Exception e) {
+      log.info(e.getMessage());
+    } }
+
+  private String getMainMenu()
+  {
+    StringBuffer buffer = new StringBuffer();
+    buffer.append("您好，如需人工服务，请联系：").append("\n\n");
+    buffer.append("1  宋丕杰：18501335305").append("\n");
+    buffer.append("2  张XXXX：132XXXXXXXX").append("\n");
+    buffer.append("3  赵XXXX：132XXXXXXXX").append("\n");
+    buffer.append("4  刘XXXX：132XXXXXXXX").append("\n");
+    buffer.append("5  王XXXX：132XXXXXXXX").append("\n");
+    return buffer.toString();
+  }
+
+  public void checkToken(HttpServletRequest request, HttpServletResponse response)
+    throws IOException
+  {
+    String signature = request.getParameter("signature");
+
+    String echostr = request.getParameter("echostr");
+
+    String timestamp = request.getParameter("timestamp");
+
+    String nonce = request.getParameter("nonce");
+
+    String[] str = { this.TOKEN, timestamp, nonce };
+    Arrays.sort(str);
+    String bigStr = str[0] + str[1] + str[2];
+
+    String digest = new SHA1().getDigestOfString(bigStr.getBytes()).toLowerCase();
+
+    if (digest.equals(signature))
+      response.getWriter().print(echostr);
+  }
 }
