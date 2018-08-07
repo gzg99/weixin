3d51ebbe7d7918e274b800f84e25175096cd9ca0
gzg_99@sina.com <gzg_99@sina.com>
2018-08-07 22:17:32 +0800

    tijiao

--- src/com/yq/controller/FileCtrl.java
+++ src/com/yq/controller/FileCtrl.java
@@ -41,4 +41,62 @@ public class FileCtrl {
 	    System.out.println(url);
 	    return url;
 	}
+	
+	@ResponseBody
+	@RequestMapping({"/upload1.html"})
+	public Object upload1(@RequestParam MultipartFile file1, HttpServletRequest request) {
+		String realpath = request.getSession().getServletContext().getRealPath("");
+		String path = "";
+		if (realpath.contains("\\"))
+			path = realpath.substring(0, realpath.lastIndexOf("\\")) + "/upload";
+		else {
+			path = realpath.substring(0, realpath.lastIndexOf("/")) + "/upload";
+		}
+	    System.out.println("path=" + path);
+	    String fileName = new Date().getTime() + ".png";
+	
+	    File targetFile = new File(path, fileName);
+	    if (!targetFile.exists()) {
+	      targetFile.mkdirs();
+	    }
+	
+	    try {
+	      file1.transferTo(targetFile);
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	    String link = WxUtil.link;
+	    String url = link + "/upload/" + fileName;
+	    System.out.println(url);
+	    return url;
+	}
+	
+	@ResponseBody
+	@RequestMapping({"/upload2.html"})
+	public Object upload2(@RequestParam MultipartFile file2, HttpServletRequest request) {
+		String realpath = request.getSession().getServletContext().getRealPath("");
+		String path = "";
+		if (realpath.contains("\\"))
+			path = realpath.substring(0, realpath.lastIndexOf("\\")) + "/upload";
+		else {
+			path = realpath.substring(0, realpath.lastIndexOf("/")) + "/upload";
+		}
+	    System.out.println("path=" + path);
+	    String fileName = new Date().getTime() + ".png";
+	
+	    File targetFile = new File(path, fileName);
+	    if (!targetFile.exists()) {
+	      targetFile.mkdirs();
+	    }
+	
+	    try {
+	      file2.transferTo(targetFile);
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	    String link = WxUtil.link;
+	    String url = link + "/upload/" + fileName;
+	    System.out.println(url);
+	    return url;
+	}
 }
