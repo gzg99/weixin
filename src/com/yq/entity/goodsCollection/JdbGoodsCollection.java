5b090d09102e73491a20731dcfed54c017cb6e0f
kaixuan1314 <kaixuan.jiang@foxmail.com>
2018-11-28 15:34:26 +0800

    商品收藏bean

---
+++ src/com/yq/entity/goodsCollection/JdbGoodsCollection.java
@@ -0,0 +1,53 @@
+package com.yq.entity.goodsCollection;
+
+public class JdbGoodsCollection {
+    private String id;
+
+    private Integer goodsId;
+
+    private String oppenId;
+
+    private String type;
+
+    private String createTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Integer goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getOppenId() {
+        return oppenId;
+    }
+
+    public void setOppenId(String oppenId) {
+        this.oppenId = oppenId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+}
